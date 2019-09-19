582139d4fe3f1a743bce8a3af4cfdf71866eb357
Sumandeep kaur <ksumanmaan3952@gmail.com>
2019-09-19 11:30:40 -0400

    R1 RED commit

---
+++ MidTermUITests/MidTermUITests.swift
@@ -0,0 +1,43 @@
+//
+//  MidTermUITests.swift
+//  MidTermUITests
+//
+//  Created by MacStudent on 2019-09-19.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import XCTest
+
+class MidTermUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    func calculateTotalInfected(int day)
+    {
+        var  day =  [1,2,3]
+        // checking the number of days
+        let size = day.count
+        if( size <=0)
+        {
+            return -1
+        }
+    }
+}
